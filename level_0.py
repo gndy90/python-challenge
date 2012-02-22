b9fa28b092daaf53f8e1e61e8e127f01e09619fb
hanks <550562236zh@sina.com>
2012-02-22 18:07:09 +0800

    change file names in order to be consistant

---
+++ level_0.py
@@ -0,0 +1,5 @@
+print 2**38
+
+#274877906944L
+
+#url: http://www.pythonchallenge.com/pc/def/274877906944.html
---
+++ level_3.py
@@ -0,0 +1,23 @@
+def getPageSourceFromUrl(url):
+    import urllib
+    sock = urllib.urlopen(url)
+    htmlSource = sock.read()
+    sock.close()
+    return htmlSource
+
+def findLetterFromSourceString(src):
+    import re
+    res = []
+    pattern = re.compile(r"""
+    [^A-Z][A-Z]{3}       # only three big bodyguard at the left side
+    ([a-z])          # only a small letter at the middle
+    [A-Z]{3}[^A-Z]       # only another three big bodyguard at the right size
+    """, re.VERBOSE)
+    # finds is a list of tuple that matches all the strings
+    res = pattern.findall(src)
+    return "".join(res)
+
+if __name__ == "__main__":
+    pageSource = getPageSourceFromUrl("http://www.pythonchallenge.com/pc/def/equality.html");
+    print findLetterFromSourceString(pageSource)
+# url: http://www.pythonchallenge.com/pc/def/linkedlist.php
